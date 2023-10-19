df424a57e64f6edacbab6c753586c62af36d4d68
unknown <edward.c.morrow@gmail.com>
2023-10-19 09:40:31 -0700

    updated schema to have table creation in separate sql file

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,26 +1,3 @@
--- DROP TABLES
-
-DROP TABLE weight;
-DROP TABLE spc;
-DROP TABLE mr;
-
--- CREATE TABLES
-
-CREATE TABLE weight (
-	entry_date date,
-	weight DEC(10,1),
-	note varchar(255)
-);
-
-CREATE TABLE spc (
-	ucl DEC(10,1),
-	lcl DEC(10,1),
-	center DEC(10,1),
-	average_mr DEC(10,3)
-);
-
-CREATE TABLE mr (moving_range DEC(10,1));
-
 -- Create Indices
 
 CREATE UNIQUE INDEX index_id ON weight (entry_date);
